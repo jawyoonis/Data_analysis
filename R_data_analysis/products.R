57f528c2f5015bf32f52b2a1502f446a1bdfe5dc
Aw Yonis <awyoonisj@gmail.com>
2020-08-24 00:38:28 -0400

    R files and code added

--- R_data_analysis/products.R
+++ R_data_analysis/products.R
@@ -4,7 +4,7 @@ Product <-read.table('C:/Users/jamal_aw_yonis/Desktop/Data_analysis/R_data_analy
                       , header = TRUE, sep = "/t")
 
 
-str(Product)
+str(Product) 
 customer <- read.csv('C:/Users/jamal_aw_yonis/Desktop/Data_analysis/Customer.csv', header = TRUE)
 View(customer)
 y <- table(customer$Region) # frequence of different categoriesview
@@ -13,5 +13,10 @@ y
 barplot(y) 
 barplot(y[order(y)]) # this orders the bars from shortest to tallest
 barplot(y[order(y)], col = c('red', 'green', 'blue', 'yellow'))
-barplot(y[order(y)], horiz = TRUE, col = c('red', 'green', 'blue', 'yellow'))
+barplot(y[order(y)], horiz = TRUE, col = c('red', 'green', 'blue', 'yellow'), 
+        main = "frequency Regions")
+
+hist(customer$Age)
+hist(customer$Age, breaks = 5)
+hist(customer$Age, breaks = c(0,40,60,100), freq = TRUE)
 
